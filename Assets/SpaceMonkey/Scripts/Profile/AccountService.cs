9aee0ee108dcc1cad013fa2c36ca6406438c8720
Arman Karapetyan <arman@doublecoconut.com>
2025-08-05 12:19:14 +0400

    removed unnecessary packages, installed from nuget newtonsoftjson

--- Assets/SpaceMonkey/Scripts/Profile/AccountService.cs
+++ Assets/SpaceMonkey/Scripts/Profile/AccountService.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
 using Cysharp.Threading.Tasks;
-using Unity.Plastic.Newtonsoft.Json;
+using Newtonsoft.Json;
 using UnityEngine;
 using Logger = DCLogger.Runtime.Logger;
 
