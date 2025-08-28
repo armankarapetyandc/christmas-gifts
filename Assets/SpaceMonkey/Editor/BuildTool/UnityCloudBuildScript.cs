84f54189dd9b91dbdb64d43b4b169bc764bc8a62
Arman Karapetyan <arman@doublecoconut.com>
2025-08-28 11:22:57 +0400

    builder will use BuildManifestObject from unity cloud build

--- Assets/SpaceMonkey/Editor/BuildTool/UnityCloudBuildScript.cs
+++ Assets/SpaceMonkey/Editor/BuildTool/UnityCloudBuildScript.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 using SpaceMonkey.Editor.BuildTool.Exceptions;
 using UnityEditor;
 using UnityEngine;
@@ -8,19 +9,36 @@ namespace SpaceMonkey.Editor.BuildTool
     public class UnityCloudBuildScript
     {
         private const string APP_VERSION_KEY = "APP_VERSION";
-        private const string APP_BUILD_NUMBER_KEY = "APP_BUILD_NUMBER";
 
-        public static void Build()
+        private static JsonSerializerSettings _serializerSettings { get; } = new JsonSerializerSettings
+        {
+            ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver(),
+            NullValueHandling = NullValueHandling.Include, // Include null values in the output
+            Formatting = Formatting.Indented // Makes the JSON output more readable
+        };
+
+        public static void Build(
+#if UNITY_CLOUD_BUILD
+            UnityEngine.CloudBuild.BuildManifestObject manifest
+#endif
+        )
         {
             try
             {
                 Log("Running custom build script...");
+#if UNITY_CLOUD_BUILD
+                Log($"Build Manifest: {JsonConvert.SerializeObject(manifest, _serializerSettings)}");
+#endif
                 var version = GetEnvironmentVariable(APP_VERSION_KEY);
-                var buildNumber = GetEnvironmentVariable(APP_BUILD_NUMBER_KEY);
-                Log($"Version: {version}, Build Number: {buildNumber}");
-
+                Log($"Version: {version}");
                 PlayerSettings.bundleVersion = version;
+
+#if UNITY_CLOUD_BUILD
+                var buildNumber = manifest.GetValue<int>("buildNumber");
+                Log($"Build Number: {buildNumber}");
                 ApplyBuildVersion(buildNumber);
+#endif
+                Log("Unity Cloud will run his build script...");
             }
             catch (Exception e)
             {
@@ -29,21 +47,22 @@ namespace SpaceMonkey.Editor.BuildTool
             }
         }
 
-        private static void ApplyBuildVersion(string buildNumber)
+        private static void ApplyBuildVersion(int buildNumber)
         {
             var currentTarget = EditorUserBuildSettings.activeBuildTarget;
             switch (currentTarget)
             {
                 case BuildTarget.Android:
-                    if (!int.TryParse(buildNumber, out int bundleVersionCode))
-                        throw new InvalidBuildNumberException(buildNumber);
-                    PlayerSettings.Android.bundleVersionCode = bundleVersionCode;
-                    Log($"Android version code set to: {bundleVersionCode}");
+                    PlayerSettings.Android.bundleVersionCode = buildNumber;
+                    Log($"Android bundle version code set to: {buildNumber}");
                     return;
                 case BuildTarget.iOS:
-                    PlayerSettings.iOS.buildNumber = buildNumber;
+                    PlayerSettings.iOS.buildNumber = buildNumber.ToString();
                     Log($"iOS build number set to: {buildNumber}");
                     break;
+                default:
+                    throw new NotImplementedException(
+                        $"Apply Build Version not implemented for the current target: {currentTarget}");
             }
         }
 
