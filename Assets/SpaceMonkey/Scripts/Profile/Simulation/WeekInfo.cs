dc2043bc04be027bac8c53ada50d5fb4e5006cd4
Arman Karapetyan <arman@doublecoconut.com>
2025-09-01 16:52:45 +0400

    Week store in account

---
+++ Assets/SpaceMonkey/Scripts/Profile/Simulation/WeekInfo.cs
@@ -0,0 +1,24 @@
+﻿namespace SpaceMonkey.Scripts.Profile.Simulation
+{
+    public struct WeekInfo
+    {
+        public string Id { get; set; }
+        public int Week { get; set; }
+        public int NeededCap { get; set; }
+        public OrderInfo[] Orders { get; set; }
+        
+    }
+    public struct OrderInfo
+    {
+        public string CharacterId { get; set; }
+        public int Mood { get; set; }
+        public ProductOrderInfo[]  Products { get; set; }
+    }
+
+    public struct ProductOrderInfo
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
+
