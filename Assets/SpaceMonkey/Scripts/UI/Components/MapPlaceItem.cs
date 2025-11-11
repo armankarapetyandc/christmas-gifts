9252b083f8c6390a6f067bd877700ada9aed4bc9
Arman Karapetyan <arman@doublecoconut.com>
2025-11-11 23:16:14 +0400

    map view changes

--- Assets/SpaceMonkey/Scripts/UI/Components/MapPlaceItem.cs
+++ Assets/SpaceMonkey/Scripts/UI/Components/MapPlaceItem.cs
@@ -1,6 +1,7 @@
 using System;
 using R3;
 using SpaceMonkey.Scripts.Configs.Map;
+using SpaceMonkey.Scripts.Core.SimpleSerializableDictionary;
 using SpaceMonkey.Scripts.UI.Asset.Database;
 using TMPro;
 using UnityEngine;
@@ -8,45 +9,65 @@ using UnityEngine.UI;
 
 namespace SpaceMonkey.Scripts.UI.Components
 {
+    public enum MapPlaceState
+    {
+        Active,
+        Selected,
+        Locked,
+        Open,
+        Event,
+        Inactive
+    }
     public class MapPlaceItem : MonoBehaviour
     {
         [SerializeField] private RectTransform rectTransform;
         [SerializeField] private TextMeshProUGUI nameText;
-        [SerializeField] private Sprite lockedIcon;
-        [SerializeField] private Image iconImage;
-        [SerializeField] private Image background;
         [SerializeField] private Button button;
+
+        [SerializeField] private Image pinImage;
+        [SerializeField] private Image iconRootImage;
+        [SerializeField] private Image contentImage;
+        [SerializeField] private SerializableDictionary<MapPlaceState, Sprite> statesMap;
+        
         private SpriteVisualAsset _visualAsset;
         private IMapPlace _place;
         public Observable<IMapPlace> OnClickAsObservable() => button.OnClickAsObservable().Select(_ => _place);
+
+
+        public void SetMapPlaceState(MapPlaceState placeState)
+        {
+            var map = statesMap.ToDictionary();
+            if (!map.TryGetValue(placeState, out var state))
+            {
+                Debug.LogError($"Unable to find pin state image for state: {placeState}");
+                return;
+            }
+
+            pinImage.sprite = state;
+            iconRootImage.gameObject.SetActive(placeState != MapPlaceState.Locked);
+        }
+
+
+        public void SetPlaceContent(Sprite sprite)
+        {
+            contentImage.sprite = sprite;
+        }
+        public void SetBackgroundColor(Color color)
+        {
+            iconRootImage.color = color;
+        }
+        
         public void SetPlaceName(string placeName)
         {
-            var a=System.Text.RegularExpressions.Regex.Replace(placeName, @"\s+", " ").Trim();
-            nameText.SetText(a);
+            var formatted=System.Text.RegularExpressions.Regex.Replace(placeName, @"\s+", " ").Trim();
+            nameText.SetText(formatted);
         }
 
         public void SetPosition(Vector2 position)
         {
             rectTransform.anchoredPosition = position;
         }
-
-        public void SetBackgroundColor(Color color)
-        {
-            background.color = color;
-        }
         
-
-        public void SetIcon(SpriteVisualAsset visualAsset)
-        {
-            _visualAsset = visualAsset;
-            iconImage.sprite = visualAsset?.Sprite;
-        }
-
-        public void SetLocked(bool state)
-        {
-            iconImage.sprite = state ? lockedIcon : _visualAsset?.Sprite;
-        }
-
         public void SetPlace(IMapPlace place)
         {
             _place = place;
