b354b4d792a37ad095de3069ed5cbd43c84c0d3e
Arman Karapetyan <arman@doublecoconut.com>
2025-11-12 00:18:10 +0400

    map canvas target positioning calculation

--- Assets/SpaceMonkey/Scripts/UI/Components/CanvasPanZoom.cs
+++ Assets/SpaceMonkey/Scripts/UI/Components/CanvasPanZoom.cs
@@ -363,6 +363,32 @@ namespace SpaceMonkey.Scripts.UI.Components
             SetPosition(seeeettl);
         }
 
+
+        public RectTransform target;
+
+        [ContextMenu("set to target")]
+        public void settotarget()
+        {
+            NavigateToTarget(target);
+        }
+
+        public void NavigateToTarget(RectTransform placeItemRect)
+        {
+            var targetPosition = CalculateTargetPosition(placeItemRect.anchoredPosition);
+            SetPosition(targetPosition);
+        }
+
+        private Vector2 CalculateTargetPosition(Vector2 position)
+        {
+            var targetAnchoredPos = position;
+            var targetSizeDelta = target.sizeDelta;
+            var contentSize = rectTransform.sizeDelta;
+            var calculatedPosition = new Vector2(targetAnchoredPos.x - contentSize.x * 0.5f + targetSizeDelta.x * 0.5f,
+                targetAnchoredPos.y + contentSize.y * 0.5f - targetSizeDelta.y * 0.5f);
+            calculatedPosition *= currentScale * -1f;
+            return calculatedPosition;
+        }
+
         public float GetCurrentZoom()
         {
             return currentScale;
