f0cf764837caccdbe56489ef126e7e98acf371ba
Arman Karapetyan <arman@doublecoconut.com>
2025-12-02 00:38:47 +0400

    Hot view fix

--- Assets/SpaceMonkey/Scripts/UI/Views/Product/NewProduct/HotItemSubView.cs
+++ Assets/SpaceMonkey/Scripts/UI/Views/Product/NewProduct/HotItemSubView.cs
@@ -7,6 +7,7 @@ namespace SpaceMonkey.Scripts.UI.Views.Product.NewProduct
     public class HotItemSubView : MonoBehaviour
     {
         [SerializeField] private Button nextButton;
+        [SerializeField] private Button backButton;
         [SerializeField] private RectTransform nextButtonRect;
         [SerializeField] private Image productImage;
         [SerializeField] private TextMeshProUGUI productName;
@@ -14,6 +15,7 @@ namespace SpaceMonkey.Scripts.UI.Views.Product.NewProduct
         
         public RectTransform NextButtonRect => nextButtonRect;
         public Button NextButton => nextButton;
+        public Button BackButton => backButton;
 
 
         public void Initialize(Sprite sprite, string pName)
