fe1c6d4ff33c59bf9e070a144b52263afe76340a
Arman Karapetyan <arman.karapetyan@say.games>
2026-01-26 23:45:30 +0400

    big order congratulation accept navigaiton

--- Assets/SpaceMonkey/Scripts/UI/Views/Opportunities/BigOrderCongratulation/BigOrderCongratulationViewController.cs
+++ Assets/SpaceMonkey/Scripts/UI/Views/Opportunities/BigOrderCongratulation/BigOrderCongratulationViewController.cs
@@ -1,4 +1,6 @@
+using Cysharp.Threading.Tasks;
 using SpaceMonkey.Scripts.UI.Navigation.Bottom;
+using SpaceMonkey.Scripts.UI.Navigation.Core;
 using SpaceMonkey.Scripts.UI.Views.Opportunities.BigOrderDetails;
 using UIService.Runtime.Presenter;
 using UIService.Runtime.Presenter.Base;
@@ -7,16 +9,24 @@ namespace SpaceMonkey.Scripts.UI.Views.Opportunities.BigOrderCongratulation
 {
     public class BigOrderCongratulationViewController : BasePresenterController
     {
-        public BigOrderCongratulationViewController(PresenterService presenterService) : base(presenterService)
+        private readonly NavigationPresenterService _navigationPresenterService;
+
+        public BigOrderCongratulationViewController(PresenterService presenterService,NavigationPresenterService navigationPresenterService) : base(presenterService)
         {
+            _navigationPresenterService = navigationPresenterService;
         }
 
         internal void OnNext(MainNavigationType Type)
         {
-            PresenterService.Show<BigOrderDetailsView>(new BigOrderDetailsView.Data()
+            _navigationPresenterService.Show<MainNavigation>(new MainNavigation.Data
+            {
+                Type =Type
+            }).Forget();
+            
+            /*PresenterService.Show<BigOrderDetailsView>(new BigOrderDetailsView.Data()
             {
                 Type = Type
-            });
+            });*/
         }
     }
 }
